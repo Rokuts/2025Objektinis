49ddc5af449a82a3279a64f69df052ab81dbc33b
Rokas Venckus <rorokasvenckus@gmail.com>
2025-02-19 04:03:21 +0200

    Mediana ir vidurkis iskaitant egzus

--- main.cpp
+++ main.cpp
@@ -10,19 +10,30 @@ struct studentas{
     string vardas, pavarde;
     int egzas, suma = 0;
     double n[10];
-    double galutinisVid, galutinisMed;
 
 };
 
 double vidurkis(studentas *s, int x){
-    return ((double)(s->suma))/x;
+    double vidurkis;
+    vidurkis = ((double)(s->suma))/x;
+    
+    return vidurkis * 0.4 + (s->egzas * 0.6);
+}
+
+double mediana(studentas *s, int x){
+    double mediana;
+    sort(&s->n[0],&s->n[x]);
+    if(x%2==0)
+        mediana = (s->n[x/2]+s->n[x/2-1])/2;
+    else mediana = (s->n[x/2]);
+    return mediana * 0.4 + (s->egzas * 0.6);
 }
 
 int main()
 {
 
     studentas S[10];
-    int n, x, y, *dydis;
+    int n, x, y;
 
     cout << "Kiek yra studentu (max 10)?" << endl;
     cin >> y;
@@ -32,7 +43,6 @@ int main()
     cin >> x;
     cout << endl;
 
-    dydis = new int [y];      //Vardo/Pavardes ilgio masyvas
 
     int ilgiausias_vardas=6, ilgiausia_pavarde=7;
     for(int i = 0; i < y; i++){
@@ -52,8 +62,16 @@ int main()
         cout << endl;
     }
 
-     
-    cout << left << setw(ilgiausia_pavarde + 3) << "Pavarde" << setw(ilgiausias_vardas + 3) << "Vardas" << setw(17) << "Galutinis (Vid.)" << endl;
+    int t;
+    cout << "Jei lenteleje norite galutinio vidurkio spauskite 0, jei medianos - 1" << endl;
+    cin >> t;
+    cout << endl; 
+
+    if(t==0){
+        cout << left << setw(ilgiausia_pavarde + 3) << "Pavarde" << setw(ilgiausias_vardas + 3) << "Vardas" << setw(17) << "Galutinis (Vid.)" << endl;
+    }
+    else cout << left << setw(ilgiausia_pavarde + 3) << "Pavarde" << setw(ilgiausias_vardas + 3) << "Vardas" << setw(17) << "Galutinis (med.)" << endl;
+
     
     for(int i = 0; i < (ilgiausia_pavarde+ilgiausias_vardas+6+17); i++){        //pridetu pakankamai "-", kad gerai atrodytu lentele;
         cout<<"-";
@@ -61,7 +79,10 @@ int main()
     cout << endl;
 
     for(int i = 0; i < y; i++){
-        cout << left << setw(ilgiausia_pavarde + 3) << S[i].pavarde << setw(ilgiausias_vardas + 3) << S[i].vardas << setw(17) << vidurkis(&S[i],x) << endl;
+        if(t==0){
+            cout << left << setw(ilgiausia_pavarde + 3) << S[i].pavarde << setw(ilgiausias_vardas + 3) << S[i].vardas << setw(17) << vidurkis(&S[i],x) << endl;
+        }
+        else cout << left << setw(ilgiausia_pavarde + 3) << S[i].pavarde << setw(ilgiausias_vardas + 3) << S[i].vardas << setw(17) << mediana(&S[i],x) << endl;
     }
 
 
