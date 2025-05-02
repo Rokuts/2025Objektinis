9afc737faf8ceda7ca5bf5721b141b9be80cd4dd
Rokas Venckus <rorokasvenckus@gmail.com>
2025-05-03 02:50:17 +0300

    Sukurtas ivedimas.h

---
+++ ivedimas.h
@@ -0,0 +1,8 @@
+#pragma once
+
+#include "headers.h"
+#include "studentas.h"
+
+void vardo_pavardes_ivedimas(vector<studentas> &S, studentas &s, int &ilgiausias_vardas, int &ilgiausia_pavarde);
+void ivedimas_ranka(vector<studentas> &S, int &ilgiausias_vardas, int &ilgiausia_pavarde);
+void generuojami_pazymiai(vector<studentas> &S, int &ilgiausias_vardas, int &ilgiausia_pavarde);
