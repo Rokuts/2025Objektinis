aafb8ef0fef9b17cbf4cb0458e24481b1bfa7da3
Rokas Venckus <rorokasvenckus@gmail.com>
2025-05-03 02:24:35 +0300

    sukurtas apskaiciuotas_studentas.cpp

--- main.cpp
+++ main.cpp
@@ -22,15 +22,7 @@ bool ar_tik_raides(const string& zodis) {
 
 
 
-//Konvertuoja studenta i apskaiciuota studenta
-apskaiciuotas_studentas apskaiciuoti_stud(studentas stud){
-    apskaiciuotas_studentas S;
-    S.studentas = stud;
-    S.vidurkis = vidurkis(&stud);
-    S.mediana = mediana(&stud);
-    return S;
-    
-}
+
 
 static const vector<string> vardai = {
 
